58bc19ba3c70701a0b3c5296acdbb2c2b7191b86
kingakurzawa <j.kingakurzawa@gmail.com>
2020-05-12 16:40:49 +0100

    corrected code in html, downloaded html elements to js

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 let buttonAdd = document.querySelector('.addAccount');
 let wrapper = document.querySelector('.wrapper');
+let username = document.querySelector('#username');
+let password = document.querySelector('#password');
+let confirmPass = document.querySelector('#confirmPass');
+let email = document.querySelector('#email');
+let error = document.querySelector('.error');
+let clearBtn = document.querySelector('.clear');
+let sendBtn = document.querySelector('.send');
+let succesInfo = document.querySelector('.succesInfo');
 
-buttonAdd.addEventListener('click', ()=> {
+
+clearBtn.addEventListener('clear', e => {
+  e.preventDefault();
+
+  [username,password,confirmPass,email].forEach(el => {
+    el.value = ''
+  })
+})
+buttonAdd.addEventListener('click', e => {
+  e.preventDefault();
   buttonAdd.style.display = 'none';
   wrapper.style.display = 'flex'
 })
